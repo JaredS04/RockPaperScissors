787a7bcea12a3dcb36820a8ac8564f6bf00fbef9
NovaMarauder <105817822+NovaMarauder@users.noreply.github.com>
2024-02-20 09:17:48 -0800

    added wins and losses aswell as fixed the choices to put them in a list of strings rather than individual variables. Computer randomly returns a 1/3 strings

--- code.js
+++ code.js
@@ -1,27 +1,44 @@
-var rock = "rock";
-var paper = "paper";
-var scissors = "scissors";
 
+let choice = ["rock", "paper", "scissors"];
 function getComputerChoice() 
   {
-  return rock, paper, scissors;
+    return choice[Math.floor(Math.random())] //randomly lets computer generate a choice
   }
 
-function playGame()
-{
+
   function playRound(playerSelection, computerSelection)
   {
+    console.log(playRound(playerSelection, computerSelection))
     // code here // scissors beats paper, paper beats rock, rock beats scissors
     
     // Get the user prompt
-    let msg = prompt("pick rock, paper or scissors")
-    // if the user prompt is rock and the computer is paper then the computer wins
-    // if the user prompt is sciccors and the computer is paper then user wins
-    // if the user prompt is rock and the computer is scissors then user wins
+    // Ai gets the user prompt and randomly picks 1/3 variables
+    // if ai picks the same choice say draw
+    if (playerSelection === computerSelection) // Displays player & computer choices and decides who wins. 
+    {
+      return `it's a tie! $[playerSelection]`; 
+    }
+    // if one picks rock and other picks scissors, rock wins
+    else if (playerSelection === "rock" && computerSelection === "scissors")
+    {
+      return `You win! ${playerSelection} beats ${computerSelection}`; // Rock beats scissors
+    }
+    // if one picks scissors and the other picks paper, scissors wins
+    else if (playerSelection === "scissors" && computerSelection === "paper")
+    {
+      return `You win! ${playerSelection} beats ${computerSelection}`; // Scissors beats paper
+    }
+    // if one picks paper and the other picks rock, paper wins
+    else if (playerSelection === "paper" && computerSelection === "rock")
+    {
+      return `You win! ${playerSelection} beats ${computerSelection}`; // Paper beats rock
+    }
+    else
+    {
+      return `You lose! ${computerSelection} beats ${playerSelection}`; // Player loses
+    }
   }
+  const playerSelection = prompt("rock, paper, or scissors?").toUpperCase;
+  const computerSelection = getComputerChoice(choice);
+  
 
-}
-
-const playerSelection = rock;
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection))
